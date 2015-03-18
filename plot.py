044a81965b1a7b401964e9f3fa4f36be0a12e1ad
Eduardo Otubo <eduardo.otubo@profitbricks.com>
2015-03-18 10:10:09 +0100

    plot.py: lack of support for both R and W operations
    plot.py was trying to grab configuration information from tepmlate.ini,
    but that does not contain information about configurations anymore (it
    use to do in the past). Instead, the script is now parsing the 'bs' and
    the 'iodepth' and is generating all the possible configurations in order
    to read the results from the csv files.
    
    Trivial:
     * Removing deprecated comments
     * Adding the label 'configuration' to the X axis
     * Saving the image to the dislk

--- plot.py
+++ plot.py
@@ -13,21 +13,35 @@ results_file0 = sys.argv[1]
 results_file1 = sys.argv[2]
 config_file = sys.argv[3]
 
+global_options = {}
 c = ConfigParser.RawConfigParser(allow_no_value=True)
 c.read(config_file)
+s = c.sections()[0]
+o = c.options(s)
 config = []
-ordered_result0 = []
-ordered_result1 = []
-for s in c.sections():
-    if s == "global":
-        continue
-    config.append(s)
+for opt in o:
+    value = c.get(s, opt)
+    global_options[opt] = value
+bss = global_options['bs'].split(' ')
+iodepths = global_options['iodepth'].split(' ')
+for bs in bss:
+    for iodepth in iodepths:
+        if global_options['rw'] == "rw" or global_options['rw'] == "randrw":
+            config.append("B%sI%sR" % (bs, iodepth))
+            config.append("B%sI%sW" % (bs, iodepth))
+        elif global_options['rw'] == "read" or global_options['rw'] == "randread":
+            config.append("B%sI%sR" % (bs, iodepth))
+        elif global_options['rw'] == "write" or global_options['rw'] == "randwrite":
+            config.append("B%sI%sW" % (bs, iodepth))
 
 reader = csv.reader(open(results_file0, 'rb'))
 results0 = dict(x for x in reader)
 
 reader = csv.reader(open(results_file1, 'rb'))
 results1 = dict(x for x in reader)
+
+ordered_result0 = []
+ordered_result1 = []
 
 for c in config:
     value0 = results0[c].replace("[", "")
@@ -46,14 +60,11 @@ rects1 = ax.bar(ind, tuple(ordered_result0), width, color='b')
 rects2 = ax.bar(ind+width, tuple(ordered_result1), width, color='r')
 
 ax.set_ylabel('iops')
-#ax.set_title('')
+ax.set_title('configuration')
 ax.set_xticks(ind+width)
 ax.set_xticklabels(tuple(config), rotation='vertical')
 
-#ax.legend( (rects1[0], rects2[0]), ('Men', 'Women') )
-
 def autolabel(rects):
-    # attach some text labels
     for rect in rects:
         height = rect.get_height()
         ax.text(rect.get_x()+rect.get_width()/2., 1.05*height, '%d'%int(height),
@@ -61,5 +72,5 @@ def autolabel(rects):
 
 autolabel(rects1)
 autolabel(rects2)
+plt.savefig("result.png")
 plt.show()
-#plt.savefig("graph1.png")
