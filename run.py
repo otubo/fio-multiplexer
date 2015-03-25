2e53a9757f08d42911874d3875df581d6b7c80d8
Eduardo Otubo <eduardo.otubo@profitbricks.com>
2015-03-25 15:54:44 +0100

    run.py: extend data structures to support bw, lat and cpu
    All internal data structures are now extended and support the additional
    information to be analyzed: band width, latency and cpu usage.
    
    Signed-off-by: Eduardo Otubo <eduardo.otubo@profitbricks.com>

--- run.py
+++ run.py
@@ -23,11 +23,17 @@ timestamp=datetime.datetime.now().strftime("%Y%m%d%H%M")
 # global variables
 x = 0
 # dictionary that is in the format:
-#   {'config_name': [list, of, results]}
-results=OrderedDict()
+#   {'config_name': [list, of, iops]}
+iops=OrderedDict()
+bw=OrderedDict()
+lat=OrderedDict()
+cpu=OrderedDict()
 # dictionary that is in the format:
 #   {'config_name': 'average for this config'}
-average_results=OrderedDict()
+average_iops=OrderedDict()
+average_bw=OrderedDict()
+average_lat=OrderedDict()
+average_cpu=OrderedDict()
 # dictionary that holds all global options defined in the [global] section of
 # the template.ini file.
 global_options = {}
@@ -79,8 +85,8 @@ def parse_lines(lines, opt):
 
 # create a new configuration file in the for "out/000%d.ini" for each
 # configuration generated. Also appends the new filename to the array
-# filenames, declares a new empty array in the dictionary 'results'
-# and a new empty array to average_results dictionary.
+# filenames, declares a new empty array in the dictionary 'iops'
+# and a new empty array to average_iops dictionary.
 def create_config_file(bs, iodepth, rw, i):
     config = ConfigParser.RawConfigParser(allow_no_value=True)
     config.add_section("global")
@@ -113,8 +119,14 @@ def create_config_file(bs, iodepth, rw, i):
     remove_whitespace_from_assignments(filename)
 
     filenames.append(filename)
-    results[section] = []
-    average_results[section] = []
+    iops[section] = []
+    bw[section] = []
+    lat[section] = []
+    cpu[section] = []
+    average_iops[section] = []
+    average_bw[section] = []
+    average_lat[section] = []
+    average_cpu[section] = []
 
 # open template.ini, grab all global options and transform the list of block
 # sizes and iodepths into interable arrays
@@ -154,7 +166,7 @@ k=0
 print "Starting benchmarks runs:"
 for filename in filenames:
     i=0
-    printn(results.keys()[k])
+    printn(iops.keys()[k])
     while i < nruns:
         # TODO: change this to a non hard-coded path
         #subprocess.call("rm -f ../disk_test/*.0", shell=True)
@@ -166,25 +178,55 @@ for filename in filenames:
         #ret = parse_lines(output_lines, "iops")
 
         # the following line is here just for testing purposes :)
-        ret=math.floor(random.random()*100000)
+        ret_iops=math.floor(random.random()*100000)
+        ret_bw=math.floor(random.random()*100000)
+        ret_lat=math.floor(random.random()*100000)
+        ret_cpu=math.floor(random.random()*100000)
 
-        if ret == -1:
+        if ret_iops == -1 or ret_bw == -1 or ret_lat == -1 or ret_cpu == -1:
             printn("!")
             continue
         else:
-            results[results.keys()[k]].append(int(ret))
+            iops[iops.keys()[k]].append(int(ret_iops))
+            bw[bw.keys()[k]].append(int(ret_bw))
+            lat[lat.keys()[k]].append(int(ret_lat))
+            cpu[cpu.keys()[k]].append(int(ret_cpu))
         printn("*")
         i+=1
     print "DONE"
-    # calculate the floor of the average and append to average_results
+    # calculate the floor of the average and append to average_iops
     # dictionary
-    average_results[results.keys()[k]].append(
-            int(math.floor(sum(results[results.keys()[k]])/
-                float(len(results[results.keys()[k]])))))
+    average_iops[iops.keys()[k]].append(
+            int(math.floor(sum(iops[iops.keys()[k]])/
+                float(len(iops[iops.keys()[k]])))))
+
+    average_bw[bw.keys()[k]].append(
+            int(math.floor(sum(bw[bw.keys()[k]])/
+                float(len(bw[bw.keys()[k]])))))
+
+    average_lat[lat.keys()[k]].append(
+            int(math.floor(sum(lat[lat.keys()[k]])/
+                float(len(lat[lat.keys()[k]])))))
+
+    average_cpu[cpu.keys()[k]].append(
+            int(math.floor(sum(cpu[cpu.keys()[k]])/
+                float(len(cpu[cpu.keys()[k]])))))
     k+=1
 
 # TODO: perhaps saving into chunks is better, imagine if, for some god forsaken
-# reason, the script breaks and all the previous results are lost :/
-writer = csv.writer(open('%s.csv' % timestamp, 'wb'))
-for key, value in average_results.items():
-       writer.writerow([key, value])
+# reason, the script breaks and all the previous iops are lost :/
+writer_iops = csv.writer(open('%s_iops.csv' % timestamp, 'wb'))
+for key, value in average_iops.items():
+       writer_iops.writerow([key, value])
+
+writer_bw = csv.writer(open('%s_bw.csv' % timestamp, 'wb'))
+for key, value in average_bw.items():
+       writer_bw.writerow([key, value])
+
+writer_lat = csv.writer(open('%s_lat.csv' % timestamp, 'wb'))
+for key, value in average_lat.items():
+       writer_lat.writerow([key, value])
+
+writer_cpu = csv.writer(open('%s_cpu.csv' % timestamp, 'wb'))
+for key, value in average_cpu.items():
+       writer_cpu.writerow([key, value])
