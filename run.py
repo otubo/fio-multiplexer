5557b3e309bce2e780f8a6118558d29e7f65cb01
Eduardo Otubo <eduardo.otubo@profitbricks.com>
2015-03-30 16:04:56 +0200

    run.py: adding "dummy" mode
    "dummy" mode skips the ssh, spawning virtual machines and running fio.
    This option is only useful only for development purposes for testing
    the whole script in a faster way.
    
    Signed-off-by: Eduardo Otubo <eduardo.otubo@profitbricks.com>

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ Usage: ./run.py [[-c|--config=] <config.ini>] [[-v|--vms=] <vms.ini>] [-h|--help
                         fio tool
     -v|--vms            configuration file that describes every virtual
                         machine setup that will be envolved on the run
+    -d|--dummy          dummy mode: skip ssh and fio to faster test the whole script
     -h|--help           displays this help message
 """
 import subprocess
@@ -35,7 +36,7 @@ timestamp=datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 #      * job file: every file inside out/ folder that represents very
 #        test-case generated, that will serve as input for `fio' tool
 #        inside the virtual machine
-
+dummy = False
 # dictionary that is in the format:
 #   {'test_name': [list, of, values]}
 iops=OrderedDict()
@@ -307,7 +308,8 @@ def mount_testing_device(vm):
     c.close()
 
 def start_jobs(vm, nruns):
-    print_verbose("I", "starting jobs on virtual machine %d" % vm['id'])
+    if dummy is not True:
+        print_verbose("I", "starting jobs on virtual machine %d" % vm['id'])
     global iops
     global bw
     global lat
@@ -326,29 +328,30 @@ def start_jobs(vm, nruns):
     local_average_lat = average_lat
     local_average_cpu = average_cpu
 
-    c = paramiko.SSHClient()
-    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    c.connect('localhost', port=5000+int(vm['id']), username=vm['user'],
-            password=vm['password'], allow_agent=False, look_for_keys=False)
+    if dummy is not True:
+        c = paramiko.SSHClient()
+        c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        c.connect('localhost', port=5000+int(vm['id']), username=vm['user'],
+                password=vm['password'], allow_agent=False, look_for_keys=False)
     k=0
     for filename in filenames:
         i=0
         printn(local_iops.keys()[k])
         while int(i) < int(nruns):
             # TODO: change this to a non hard-coded path
-            stdin, stdout, stderr = c.exec_command("fio /tmp/%s" % filename)
-            output = stdout.read()
-            output_lines = string.split(output, "\n")
-            ret_iops = parse_lines(output_lines, "iops")
-            ret_bw = parse_lines(output_lines, "bw")
-            ret_lat = parse_lines(output_lines, "lat")
-            ret_cpu = parse_lines(output_lines, "cpu")
-
-            # the following line is here just for testing purposes :)
-            #ret_iops=math.floor(random.random()*100000)
-            #ret_bw=math.floor(random.random()*100000)
-            #ret_lat=math.floor(random.random()*100000)
-            #ret_cpu=math.floor(random.random()*100000)
+            if dummy is not True:
+                stdin, stdout, stderr = c.exec_command("fio /tmp/%s" % filename)
+                output = stdout.read()
+                output_lines = string.split(output, "\n")
+                ret_iops = parse_lines(output_lines, "iops")
+                ret_bw = parse_lines(output_lines, "bw")
+                ret_lat = parse_lines(output_lines, "lat")
+                ret_cpu = parse_lines(output_lines, "cpu")
+            else:
+                ret_iops=math.floor(random.random()*100000)
+                ret_bw=math.floor(random.random()*100000)
+                ret_lat=math.floor(random.random()*100000)
+                ret_cpu=math.floor(random.random()*100000)
 
             if ret_iops == -1 or ret_bw == -1 or ret_lat == -1 or ret_cpu == -1:
                 printn("!")
@@ -383,7 +386,8 @@ def start_jobs(vm, nruns):
                 int(math.floor(sum(local_cpu[local_cpu.keys()[k]])/
                     float(len(local_cpu[local_cpu.keys()[k]])))))
         k+=1
-    c.close()
+    if dummy is not True:
+        c.close()
 
     # TODO: perhaps saving into chunks is better, imagine if, for some god forsaken
     # reason, the script breaks and all the previous iops are lost :/
@@ -410,17 +414,19 @@ def start_jobs(vm, nruns):
     for key, value in local_average_cpu.items():
            writer_cpu.writerow([key, value])
 
-    stop_vm(vm)
+    if dummy is not True:
+        stop_vm(vm)
 
 def main():
     # parse command line options
     global config_file
     global vms_file
     global vms
+    global dummy
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hv:c:", ["help", "vms=",
-            "config="])
+        opts, args = getopt.getopt(sys.argv[1:], "hv:c:d", ["help", "vms=",
+            "config=", "dummy="])
     except getopt.error, msg:
         print msg
         print "for help use --help"
@@ -434,6 +440,8 @@ def main():
             vms_file=a
         elif o in ("-c", "--config"):
             config_file=a
+        elif o in ("-d", "--dummy"):
+            dummy = True
 
     #if nothing is set, go back to default
     if config_file == None:
@@ -444,15 +452,17 @@ def main():
         print_verbose("I", "No configuration given, using default vms.ini")
         vms_file = "vms.ini"
 
+    print_verbose("W", "running in dummy mode!")
     parse_main_config(config_file)
     parse_vms(vms_file)
     cleanup()
     create_all_jobs(bss, iodepths)
 
-    for vm in vms:
-        spawn_virtual_machine(vms[vm])
-        scp_job_files(vms[vm])
-        mount_testing_device(vms[vm])
+    if dummy is not True:
+        for vm in vms:
+            spawn_virtual_machine(vms[vm])
+            scp_job_files(vms[vm])
+            mount_testing_device(vms[vm])
 
     #everything is set, start `fio'
     for vm in vms:
